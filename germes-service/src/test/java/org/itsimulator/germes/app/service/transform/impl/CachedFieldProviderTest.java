201c7bb64a78847f2608c77c8114c339abd529ab
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-01-13 13:30:54 +0700

    0.2.1

--- germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
@@ -1,11 +1,18 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
+import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
+import java.util.Collections;
 
 /**
  * Verifies functionality of the {@link SimpleDTOTransformer}
@@ -13,6 +20,8 @@ import java.util.List;
  * @author Morenets
  *
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ReflectionUtil.class)
 public class CachedFieldProviderTest {
 	private FieldProvider provider;
 	
@@ -25,7 +34,7 @@ public class CachedFieldProviderTest {
 	public void testGetFieldNamesSuccess() {
 		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
 		assertFalse(fields.isEmpty());
-		assertTrue(fields.contains("value"));		
+		assertTrue(fields.contains("value"));
 	}
 
 	@Test
@@ -33,8 +42,23 @@ public class CachedFieldProviderTest {
 		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
 		List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
 		assertFalse(fields.isEmpty());
-		assertEquals(fields, fields2);		
+		assertEquals(fields, fields2);
 	}
+
+	@Test
+	public void testGetFieldNamesAreCached() {
+		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+		PowerMockito.mockStatic(ReflectionUtil.class);
+		when(ReflectionUtil.findSimilarFields(Source.class, Destination.class)).thenReturn(Collections.emptyList());
+
+		assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
+		List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+		assertFalse(fields.isEmpty());
+		assertEquals(fields, fields2);
+	}
+
+
 	
 }
 
