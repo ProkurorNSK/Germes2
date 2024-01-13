caa9bb1c5ac2076991d709526b9da696b01b1731
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-01-13 12:52:22 +0700

    0.2.0

--- germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
@@ -6,7 +6,6 @@ import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
 import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
 import org.itsimulator.germes.app.service.transform.Transformer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,18 +16,21 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class SimpleDTOTransformer implements Transformer {
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
 
+	private final FieldProvider provider;
+
+	public SimpleDTOTransformer() {
+		provider = new CachedFieldProvider();
+	}
+
 	@Override
-	public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(
-			final T entity, final Class<P> clz) {
+	public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz) {
 		checkParams(entity, clz);
 
 		P dto = ReflectionUtil.createInstance(clz);
 		// Now just copy all the similar fields
-		ReflectionUtil.copyFields(entity, dto,
-				ReflectionUtil.findSimilarFields(entity.getClass(), clz));
+		ReflectionUtil.copyFields(entity, dto, provider.getFieldNames(entity.getClass(), clz));
 		dto.transform(entity);
 
 		if (LOGGER.isDebugEnabled()) {
@@ -40,19 +42,16 @@ public class SimpleDTOTransformer implements Transformer {
 
 	private void checkParams(final Object param, final Class<?> clz) {
 		Checks.checkParameter(param != null, "Source transformation object is not initialized");
-		Checks.checkParameter(clz != null,
-				"No class is defined for transformation");
+		Checks.checkParameter(clz != null, "No class is defined for transformation");
 	}
 
 	@Override
-	public <T extends AbstractEntity, P extends BaseDTO<T>> T untransform(
-			final P dto, final Class<T> clz) {
+	public <T extends AbstractEntity, P extends BaseDTO<T>> T untransform(final P dto, final Class<T> clz) {
 		checkParams(dto, clz);
 
 		T entity = ReflectionUtil.createInstance(clz);
 
-		ReflectionUtil.copyFields(dto, entity,
-				ReflectionUtil.findSimilarFields(dto.getClass(), clz));
+		ReflectionUtil.copyFields(dto, entity, provider.getFieldNames(dto.getClass(), clz));
 		dto.untransform(entity);
 
 		if (LOGGER.isDebugEnabled()) {
